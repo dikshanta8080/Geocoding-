83976eac8a2f05ac3be13f4edb4d44be2b955c84
dikshanta <dikshantaacharya04@gmail.com>
2025-11-13 19:18:39 +0545

    initialized the project

---
+++ src/main/java/com/dikshanta/geocoding/geocoding_service/GeocodingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dikshanta.geocoding.geocoding_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocodingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dikshanta/geocoding/geocoding_service/GeocodingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikshanta.geocoding.geocoding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
